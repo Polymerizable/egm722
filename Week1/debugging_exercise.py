98e339b478b40b36283535eb0119fc7556554077
Polymerizable <binarydetritus@protonmail.com>
2024-04-12 19:12:02 +0200

    adding number of attempts
    -create attempt count variable
    -increase this variable one unit every cycle

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -2,7 +2,7 @@ import random
 
 # pick a random number for the user to guess
 rand = random.randint(1, 20)
-
+n_attemps=1
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
 
@@ -11,8 +11,9 @@ while guess != rand:  # if the guess is not equal to the random number, you have
         print('Too high. Guess again.')
     else:  # if the guess is too low, tell the user.
         print('Too low. Guess again.')
-
+    n_attemps += 1
     print('Enter a new guess: ')
     guess = int(input())
 
 print('You got it! The number was {}'.format(rand))
+print('It took you {} attempts to guess it'.format(n_attemps))
